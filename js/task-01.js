a318819c7f90ee4684c7d9f5f948c5ffa61d5dfb
BohdanBaksheiev93 <131498953+BohdanBaksheiev93@users.noreply.github.com>
2023-08-17 19:03:02 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,5 @@
 const ulCategories = document.querySelector("#categories");
-console.log(ulCategories);
 const categoryItems = document.querySelectorAll("li.item");
-console.log(categoryItems);
 console.log('Number of categories:', categoryItems.length);
 categoryItems.forEach(categoryItem => {
     const categoryName = categoryItem.querySelector("h2").textContent;
